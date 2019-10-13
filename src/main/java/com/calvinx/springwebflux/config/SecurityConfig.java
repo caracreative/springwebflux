731f78fa4ca52e921a97e1d43d4685199dc3f1dc
Calvin Cheng <linchuan.cheng@gmail.com>
2019-10-14 07:10:52 +0800

    userDetailsService allows us to login easily
    Signed-off-by: Calvin Cheng <linchuan.cheng@gmail.com>

--- src/main/java/com/calvinx/springwebflux/config/SecurityConfig.java
+++ src/main/java/com/calvinx/springwebflux/config/SecurityConfig.java
@@ -5,11 +5,26 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.reactive.EnableWebFluxSecurity;
 import org.springframework.security.config.web.server.ServerHttpSecurity;
+import org.springframework.security.core.userdetails.MapReactiveUserDetailsService;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.server.SecurityWebFilterChain;
 
 @Configuration
 @EnableWebFluxSecurity
 public class SecurityConfig {
+
+  @Bean
+  public MapReactiveUserDetailsService userDetailsService() {
+    UserDetails user = User
+      .withUsername("admin")
+      .password(passwordEncoder().encode("password"))
+      .roles("USER")
+      .build();
+    return new MapReactiveUserDetailsService(user);
+  }
 
   @Bean
   public SecurityWebFilterChain springSecurityFilterChain(ServerHttpSecurity http) { // @formatter:off
@@ -21,5 +36,11 @@ public class SecurityConfig {
         .formLogin().and();
     return http.build();
   }
+
+  @Bean
+  public PasswordEncoder passwordEncoder() {
+    return new BCryptPasswordEncoder();
+  }
+
 
 } // @formatter:on
